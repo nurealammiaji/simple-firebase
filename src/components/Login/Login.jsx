8854d83066b7b213fe67220cf3ba67cf9f60feec
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-17 23:54:56 +0600

    nav bar style modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,17 @@
 
 
 const Login = () => {
-    
+
+    const loginHandler = () => {
+        console.log("Login Button Clicked");
+    }
+
     return (
         <div>
             <h3>Login Page</h3>
             <br /><br />
             <div className="text-center">
-                <button className="bg-yellow-400 py-2 px-5 rounded-md">Login</button>
+                <button className="bg-yellow-400 py-2 px-5 rounded-md" onClick={loginHandler}>Login</button>
             </div>
         </div>
     );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="flex justify-between items-center">
-            <div>
+        <nav className="flex justify-between items-center bg-green-200 py-5">
+            <div className="ml-2">
                 <NavLink to="/"><h2 className="text-3xl font-bold">Simple Firebase</h2></NavLink>
             </div>
-            <div className="[&>*]:p-5 hover:[&>*]:bg-yellow-400 font-semibold">
+            <div className="[&>*]:px-3 [&>*]:py-2 hover:[&>*]:bg-yellow-400 hover:[&>*]:rounded-2xl font-semibold mr-3">
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/about">About</NavLink>
                 <NavLink to="/login">Login</NavLink>
