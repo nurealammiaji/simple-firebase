7cd730b18e8bd5f82325eebb2250100e74fe0b5d
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-18 03:22:47 +0600

    logout funciton added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,11 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../../firebase.init';
+import { useState } from "react";
 
 
 const Login = () => {
+
+    const [user, setUser] = useState(null);
 
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
@@ -12,12 +15,26 @@ const Login = () => {
         signInWithPopup(auth, provider)
 
         .then( result => {
-            const user = result.user;
-            console.log(user);
+            const loggedInUser = result.user;
+            console.log(loggedInUser);
+            setUser(loggedInUser);
         })
 
         .catch( error => {
-            console.log('Error', error.message);
+            console.log('Login Error', error.message);
+        })
+    }
+
+    const logoutHandler = () => {
+
+        signOut(auth)
+
+        .then(result => {
+            console.log("Logout Successfully", result);
+            setUser(null);
+        })
+        .catch(error => {
+            console.log('Logout Error', error.message);
         })
     }
 
@@ -26,8 +43,22 @@ const Login = () => {
             <h3>Login Page</h3>
             <br /><br />
             <div className="text-center">
-                <button className="bg-yellow-400 py-2 px-5 rounded-md" onClick={loginHandler}>Login</button>
+                { user ?
+                    <button className="bg-yellow-400 py-2 px-5 rounded-md" onClick={logoutHandler}>Logout</button> :
+                    <button className="bg-yellow-400 py-2 px-5 rounded-md" onClick={loginHandler}>Login</button>
+                }
             </div>
+            { user &&
+                <div className="w-6/12 mx-auto">
+                    <br /><br />
+                    <div>
+                        <img src={user.photoURL} alt="" />
+                    </div>
+                    <br />
+                    <h3>Name: {user.displayName}</h3>
+                    <h3>Email: {user.email}</h3>
+                </div>
+            }
         </div>
     );
 };
