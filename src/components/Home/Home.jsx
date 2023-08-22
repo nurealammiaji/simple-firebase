bf8c7e72edd9375b564e35f4e51d5334075b85b2
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-23 01:33:10 +0600

    nav bar style modified

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
-
+import image from "../../../public/firebase.png";
 
 const Home = () => {
     return (
         <div>
-            <h3>Home Page</h3>
+            <br /><br />
+            <div className="grid grid-cols-2">
+                <div>
+                    <div>
+                        <h1>Simple</h1>
+                        <h1>Firebase</h1>
+                        <h1>Authentication</h1>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vitae voluptates pariatur debitis consequatur aliquam, error necessitatibus veritatis cumque alias. Minima earum culpa nostrum corporis dolor quis maxime veritatis officia.</p>
+                    <button>Get Started</button>
+                </div>
+                <div>
+                    <img src={image} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <nav className="flex items-center justify-between py-5 bg-green-200">
+        <nav className="flex items-center justify-between py-5 bg-blue-950 rounded-2xl [&>*]:mx-5 text-white">
             <div className="ml-2">
                 <NavLink to="/"><h2 className="text-3xl font-bold">Simple Firebase</h2></NavLink>
             </div>
-            <di className="[&>*]:px-3 [&>*]:py-2 hover:[&>*]:bg-yellow-400 hover:[&>*]:rounded-2xl font-semibold mr-3">
-                <NavLink className={({ isActive}) => isActive ? "bg-yellow-400 rounded-2xl" : ""} to="/">Home</NavLink>
-                <NavLink className={({isActive}) => isActive ? "bg-yellow-400 rounded-2xl" : ""} to="/about">About</NavLink>
-                <NavLink className={({isActive}) => isActive ? "bg-yellow-400 rounded-2xl" : ""} to="/login">Login</NavLink>
+            <di className="[&>*]:px-3 [&>*]:py-2 hover:[&>*]:bg-yellow-400 hover:[&>*]:rounded-2xl font-semibold mr-3 [&>*]:mx-2 hover:[&>*]:text-black">
+                <NavLink className={({ isActive}) => isActive ? "bg-yellow-400 rounded-2xl text-black" : ""} to="/">Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? "bg-yellow-400 rounded-2xl text-black" : ""} to="/about">About</NavLink>
+                <NavLink className={({isActive}) => isActive ? "bg-yellow-400 rounded-2xl text-black" : ""} to="/login">Login</NavLink>
             </di>
         </nav>
     );
